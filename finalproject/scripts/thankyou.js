e3c158dbaafe97f39b32c76e76d15ac29a02fe06
tydn021 <tnelson9@byupathway.edu>
2026-04-07 14:21:17 -0700

    final project 3

--- finalproject/scripts/thankyou.js
+++ finalproject/scripts/thankyou.js
@@ -6,19 +6,19 @@ console.log(myInfo);
 
 console.log(myInfo.get('first'));
 console.log(myInfo.get('last'));
-console.log(myInfo.get('org-name'));
+console.log(myInfo.get('address'));
 console.log(myInfo.get('timestamp'));
-console.log(myInfo.get('membership-level'));
+console.log(myInfo.get('tour-level'));
 console.log(myInfo.get('phone'));
 console.log(myInfo.get('email'));
 
 
 document.querySelector('#results').innerHTML = `
 <p><b>Member</b>: ${myInfo.get('first')} ${myInfo.get('last')}</p>
-<p><b>Business</b>: ${myInfo.get('org-name')}</p>
+<p><b>Business</b>: ${myInfo.get('address')}</p>
 <p><b>Your Phone</b>: ${myInfo.get('phone')}</p>
 <p><b>Your Email</b>: ${myInfo.get('email')}</p>
-<p><b>Your Membership Level</b>: ${myInfo.get('membership-level')}</p>
+<p><b>Your Tour Length</b>: ${myInfo.get('tour-level')}</p>
 <p>${myInfo.get('timestamp')}</p>
 `;
 
