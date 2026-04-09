44515012dbeca1adcfb2f8a0a9a8fe6409ff1a7d
tydn021 <tnelson9@byupathway.edu>
2026-04-08 19:02:12 -0700

    Final project pass 4

--- finalproject/scripts/thankyou.js
+++ finalproject/scripts/thankyou.js
@@ -15,7 +15,7 @@ console.log(myInfo.get('email'));
 
 document.querySelector('#results').innerHTML = `
 <p><b>Member</b>: ${myInfo.get('first')} ${myInfo.get('last')}</p>
-<p><b>Business</b>: ${myInfo.get('address')}</p>
+<p><b>Address</b>: ${myInfo.get('address')}</p>
 <p><b>Your Phone</b>: ${myInfo.get('phone')}</p>
 <p><b>Your Email</b>: ${myInfo.get('email')}</p>
 <p><b>Your Tour Length</b>: ${myInfo.get('tour-level')}</p>
